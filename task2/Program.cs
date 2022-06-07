4b3e6aed25d71af491b013ab7f02f6430c4676ee
Ramka <whereisourgod@gmail.com>
2022-06-07 05:14:07 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Array (int any, int tmp)
+{
+
+    if(tmp <= any)
+    {
+        
+        Console.Write($"{tmp} ");
+        Array(any, tmp + 1);
+    }
+}
+//Показать натуральные числа от 1 до N, N задано
+Console.WriteLine("Which number?");
+int number = int.Parse(Console.ReadLine());
+int tmp = 1;
+Array(number, tmp);
