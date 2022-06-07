867fb548c9e38fd0187290072d4d1fde47d08a13
Ramka <whereisourgod@gmail.com>
2022-06-07 04:48:51 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать натуральные числа от N до 1, N задано
+Console.WriteLine("Which number?");
+int number = int.Parse(Console.ReadLine());
+void Array (int any)
+{
+    if(any > 0) 
+    {
+        Console.Write($"{any} ");
+        Array(any - 1);
+    }
+}
+Array(number);
